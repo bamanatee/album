1149b09035a9bcf6b9ba1cc3c42e1d625aaf4f91
bamanatee <alicia.naorm@gmail.com>
2026-04-01 16:38:20 +0200

    update

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
-const Images=
-function changeImage(){
-
+const Images= ["matt_maltese.webp","matt_maltese2.webp","matt_maltese3.webp"];
+const Musiques=[];
+let index=0;
+function changeImage(Images){
+    if (index<=3) {
+        index+=1;
+        document.getElementById("hers")=const
+    }
 }
+document.getElementById("switch").innerHTML = changeImage(Images[index]);
